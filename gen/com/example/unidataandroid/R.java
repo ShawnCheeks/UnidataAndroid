bce5a3b9b399b933eaf9f726735cf53e443ad409
Shawn Cheeks <cheeks@unidata.ucar.edu>
2013-06-24 13:44:35 -0600

    Added strings and started creating layout

--- gen/com/example/unidataandroid/R.java
+++ gen/com/example/unidataandroid/R.java
@@ -24,7 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int scrollView1=0x7f080000;
+        public static final int spinner1=0x7f080003;
+        public static final int textview_product_type=0x7f080002;
+        public static final int textview_welcome=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,7 +39,16 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int location_lat=0x7f050008;
+        public static final int location_lon=0x7f050009;
+        public static final int main_location=0x7f050005;
+        public static final int main_product=0x7f050004;
+        public static final int main_product_type=0x7f050003;
+        public static final int main_submit=0x7f050007;
+        public static final int main_variables=0x7f050006;
+        public static final int product_ncep_gfs_conus_80km=0x7f05000b;
+        public static final int product_type_ncep=0x7f05000a;
+        public static final int welcome=0x7f050002;
     }
     public static final class style {
         /** 
